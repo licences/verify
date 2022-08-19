add75d81686cf4f5be068a47c1cb8af0b93df32d
licences <111577179+licences@users.noreply.github.com>
2022-08-19 18:35:41 +0300

    index_02

---
+++ HCP/index.php
@@ -0,0 +1,10 @@
+<?php
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?>
+
+<php
+echo <img src="HCP_030078801.PNG" alt="Certificate">
+/>
